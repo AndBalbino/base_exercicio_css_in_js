5ec278e502b2e913cb9679930daae2b88f93a206
Anderson Balbino <andersonbalbino2212@outlook.com>
2024-03-04 23:07:50 -0300

    Dando inicio a estilização com Styled Components
    Estilizado o Cabeçalho, Formulário e o Hero.

--- src/components/FormVagas/index.tsx
+++ src/components/FormVagas/index.tsx
@@ -1,6 +1,7 @@
 import { FormEvent, useState } from 'react'
 
-import styles from './FormVagas.module.css'
+// import styles from './FormVagas.module.css'
+import styled from 'styled-components'
 
 type Props = {
   aoPesquisar: (termo: string) => void
@@ -14,18 +15,40 @@ const FormVagas = ({ aoPesquisar }: Props) => {
     aoPesquisar(termo.toLocaleLowerCase())
   }
 
+  const Form = styled.form`
+    display: grid;
+    grid-template-columns: 1fr auto;
+    background-color: var(--cor-secundaria);
+    padding: 32px;
+    border-radius: 12px;
+    margin-top: 40px;
+  `
+
+  const Input = styled.input`
+    padding: 0 16px;
+    outline-color: var(--cor-principal);
+  `
+
+  const Botao = styled.button`
+  background-color: var(--cor-principal);
+  border: 1px solid var(--cor-principal);
+  height: 40px;
+  padding: 0 16px;
+  font-size: 18px;
+  color: var(--cor-secundaria);
+  margin-left: 8px;
+  cursor: pointer;F
+  `
+
   return (
-    <form className={styles.form} onSubmit={aoEnviarForm}>
-      <input
-        className={styles.campo}
+    <Form onSubmit={aoEnviarForm}>
+      <Input
         placeholder="Front-end, fullstack, node, design"
         onChange={(e) => setTermo(e.target.value)}
         type="search"
       />
-      <button className={styles.btnPesquisar} type="submit">
-        Pesquisar
-      </button>
-    </form>
+      <Botao type="submit">PesquisaR</Botao>
+    </Form>
   )
 }
 export default FormVagas
